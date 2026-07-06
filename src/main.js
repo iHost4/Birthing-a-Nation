bb5451c85acb9c9eb312231fb0336c4a685e7796
iHost4 <156621741+iHost4@users.noreply.github.com>
2026-07-06 18:32:25 -0500

    Update main.js: set to regular

--- src/main.js
+++ src/main.js
@@ -50,7 +50,7 @@ function Main(){
             if(day === 5 && hour >= 20){
                 setNoMoreOrders(true);
             }else{
-                setNoMoreOrders(true);
+                setNoMoreOrder(false);
             }
         };
 
