4b4c6f4af329bcaed7d523b25a06818422425ebd
iHost4 <156621741+iHost4@users.noreply.github.com>
2026-09-12 16:36:53 -0500

    Update main.js

--- src/main.js
+++ src/main.js
@@ -39,7 +39,7 @@ function Main(){
 
             //check if the current time is the Sabbath: Friday 6pm - Satuday 6pm
             if(day === 5 && hour >= 19 || day === 6 && hour < 19){ //CHANGE ACCORDING TO DLS
-                setIsSabbath(true);
+                setIsSabbath(false);
             }else{
                 setIsSabbath(false);
             }
